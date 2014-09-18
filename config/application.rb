692585e7c9a443d0b122194795ec3d56432bfff7
Daneil Wei <daneilmu1@gmail.com>
2014-09-18 10:48:01 +0800

    add secret_key_base

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module ShibaWeb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.secret_key_base = 'fa27d3c4d61662034ad8b6e4aa40168137a65da46d742f5687507f701f05d2d21daa3a1e836281dd3e135510f48cfb57e0cc7348b3dffbb9953c39955514600e'
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
